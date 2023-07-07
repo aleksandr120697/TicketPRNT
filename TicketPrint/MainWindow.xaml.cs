d1224fa1ee69d1aba762b59c52d14bead2eff986
Александр Тимашов <aleksandr120697@gmail.com>
2023-07-07 10:09:36 +0300

    Добавьте файлы проекта.

---
+++ TicketPrint/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Printing;
+using System.Windows;
+using System.Windows.Media.Animation;
+using SysTask = System.Threading.Tasks;
+
+namespace TicketPrint
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private string SaveData;
+        private static string data;
+        private static string time;
+        private static int number;
+        private bool printComplite = false;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SaveData = Properties.Settings.Default.data;
+            string NewData = DateTime.Now.ToString("dd/MM/yy");
+            if(SaveData != NewData)
+            {
+                Properties.Settings.Default.data = NewData;
+                SaveData = NewData;
+                Properties.Settings.Default.num = 0;
+                Properties.Settings.Default.Save();
+            }
+            number = Properties.Settings.Default.num;
+        }
+
+        private void SaveNumber(int number)
+        {
+            Properties.Settings.Default.num = number;
+            Properties.Settings.Default.Save();
+        }
+        //попытка в которой я хотел запускать из кода(работает ещё хужечем всё что есть сейчас.)
+        Storyboard animation;
+        private void StartAnimation()
+        {
+            try
+            {
+                if(animation == null) 
+                {
+                    
+                    animation = (Storyboard)FindResource("Animations");
+                    animation.Begin();
+                }
+                else
+                {
+                    animation.Remove();
+                    animation.Begin();
+                }
+
+           }
+           catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            number++;
+            time = DateTime.Now.ToString("HH/mm/ss");
+            data = DateTime.Now.ToString("dd/MM/yy");
+            this.numberBox.Text = number.ToString();
+            this.dataBox.Text = data.ToString();
+            this.timeBox.Text = time.ToString();
+            //StartAnimation();
+            SaveNumber(number);
+
+            var helper = new WordHelper("Documents/talon.docx");
+            var items = new Dictionary<string, string>
+            {
+                {"<NUM>",number.ToString()},
+                {"<DAT>",data },
+                {"<TIME>",time },
+            };
+            string docPath = helper.Process(items);
+            if (docPath == null)
+            {
+                if (helper.Print(docPath) == false)
+                {
+                    MessageBox.Show("При печати произошла ошибка.\n Пожалуйста сообщите об этом сотруднику. \n Ваш номер талона: " + number + "");
+                }
+                else
+                {
+                    var comp = System.Environment.MachineName;
+                    PrintServer myPrintServer = new PrintServer(@"\\DESKTOP-0F07K48" /*+ comp*/);
+                    PrintQueueCollection myPrintQueues = myPrintServer.GetPrintQueues();
+                    try
+                    {
+                        foreach (PrintQueue pq in myPrintQueues)
+                        {
+                            pq.Refresh();
+                            PrintJobInfoCollection pCollection = pq.GetPrintJobInfoCollection();
+                            foreach (PrintSystemJobInfo job in pCollection)
+                            {
+                                SpotTroubleUsingJobAttributes(job);
+                            }
+                            if (printComplite)
+                            {
+                                SysTask.Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+                            }
+
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //throw;
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void SpotTroubleUsingJobAttributes(PrintSystemJobInfo theJob)
+        {
+            if ((theJob.JobStatus & PrintJobStatus.Blocked) == PrintJobStatus.Blocked)
+            {
+                //list.Items.Add("Задание заблокировано.");
+            }
+            if (((theJob.JobStatus & PrintJobStatus.Completed) == PrintJobStatus.Completed)
+                ||
+                ((theJob.JobStatus & PrintJobStatus.Printed) == PrintJobStatus.Printed))
+            {
+                //list.Items.Add("Работа закончена. Попросите пользователя перепроверить все выходные ячейки и убедиться, что проверяется правильный принтер.");
+                printComplite = true;
+            }
+            if (((theJob.JobStatus & PrintJobStatus.Deleted) == PrintJobStatus.Deleted)
+                ||
+                ((theJob.JobStatus & PrintJobStatus.Deleting) == PrintJobStatus.Deleting))
+            {
+                //list.Items.Add( "Пользователь или кто-либо другой, обладающий правами администратора очереди, удалил задание. Оно должно быть отправлено повторно.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Error) == PrintJobStatus.Error)
+            {
+                //list.Items.Add("В работе произошла ошибка.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Offline) == PrintJobStatus.Offline)
+            {
+                //list.Items.Add("Принтер отключен от сети. Попросите пользователя подключить его к сети с помощью передней панели принтера.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.PaperOut) == PrintJobStatus.PaperOut)
+            {
+                //list.Items.Add("В принтере закончилась бумага нужного формата для выполнения задания. Попросите пользователя добавить бумагу.");
+            }
+
+            //if (((theJob.JobStatus & PrintJobStatus.Paused) == PrintJobStatus.Paused)
+            //    ||
+            //    ((theJob.HostingPrintQueue.QueueStatus & PrintQueueStatus.Paused) == PrintQueueStatus.Paused))
+            //{
+            //    HandlePausedJob(theJob);
+            //    //HandlePausedJob is defined in the complete example.
+            //}
+
+            if ((theJob.JobStatus & PrintJobStatus.Printing) == PrintJobStatus.Printing)
+            {
+                //list.Items.Add("Сейчас задание печатается.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.Spooling) == PrintJobStatus.Spooling)
+            {
+                //list.Items.Add("Сейчас работа сворачивается.");
+            }
+            if ((theJob.JobStatus & PrintJobStatus.UserIntervention) == PrintJobStatus.UserIntervention)
+            {
+                //list.Items.Add("Принтер нуждается в вмешательстве человека.");
+            }
+
+        }
+
+
+    }
+    
+}
---
+++ TicketPrint/WordHelper.cs
@@ -0,0 +1,122 @@
+﻿
+using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace TicketPrint
+{
+    internal class WordHelper
+    {
+        private FileInfo _fileInfo;
+
+        public WordHelper(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                _fileInfo = new FileInfo(filename);
+            }
+            else
+            {
+                throw new ArgumentException("Файл не найден");
+            }
+        }
+        Word.Application app = null;
+        Word.Application printingDoc = null;
+
+        internal string Process(Dictionary<string, string> items)
+        {
+
+            try
+            {
+                app = new Word.Application();
+                Object file = _fileInfo.FullName;
+                Object missing = Type.Missing;
+                app.Documents.Open(file);
+
+                foreach (var item in items)
+                {
+                    Word.Find find = GetSelection(app).Find;
+                    find.Text = item.Key;
+                    find.Replacement.Text = item.Value;
+
+                    Object wrap = Word.WdFindWrap.wdFindContinue;
+                    Object replase = Word.WdReplace.wdReplaceAll;
+
+                    find.Execute(FindText: Type.Missing,
+                        MatchCase: false,
+                        MatchWholeWord: false,
+                        MatchWildcards: false,
+                        MatchSoundsLike: missing,
+                        MatchAllWordForms: false,
+                        Forward: true,
+                        Wrap: wrap,
+                        Format: false,
+                        ReplaceWith: missing,
+                        Replace: replase);
+                }
+                string dir = _fileInfo.DirectoryName + "\\" + DateTime.Now.ToString("dd/MM/yy");
+                string fileName = DateTime.Now.ToString("HH/mm/ss") + "_" + _fileInfo.Name;
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                string newFileName = Path.Combine(dir, fileName);
+                //сохраняем отредактированный документ
+                app.ActiveDocument.SaveAs2(newFileName);
+                app.ActiveDocument.Close(SaveChanges: false);
+                //Возвращаем директорию откуда будет печататься файл
+                return newFileName;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                app.ActiveDocument.Close(SaveChanges: false);
+
+            }
+            finally
+            {
+                if (app != null)
+                    app.Quit();
+            }
+            return null;
+        }
+
+        private static Selection GetSelection(Word.Application app)
+        {
+            return app.Selection;
+        }
+
+        internal bool Print(string docPath)
+        {
+            try
+            {
+                printingDoc = new Word.Application();
+                printingDoc.Documents.Open(docPath);
+                printingDoc.ActiveDocument.PrintOut();
+                printingDoc.ActiveDocument.Close(SaveChanges: false);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                printingDoc.ActiveDocument.Close(SaveChanges: false);
+            }
+            finally
+            {
+                if (printingDoc != null)
+                {
+                    printingDoc.Quit();
+                }
+            }
+            return false;
+        }
+        
+
+    }
+}
