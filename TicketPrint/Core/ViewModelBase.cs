0fefd7935fe399a5f6447e3ea5ebac00cdfa6fc7
Александр Тимашов <aleksandr120697@gmail.com>
2023-07-11 22:07:56 +0300

    /

---
+++ TicketPrint/Core/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Core
+{
+    public  abstract partial class ViewModelBase : BaseInpc
+    {
+        private readonly Dictionary<string, object?> _properties = new();
+
+        protected T? Get<T>([CallerMemberName] string propertyName = "")
+        {
+            T? value;
+            if (_properties.TryGetValue(propertyName, out object? _prop))
+            {
+                value = (T?)_prop;
+            }
+            else
+            {
+                value = default;
+            }
+            return value;
+        }
+
+        protected void Set<T>(T? newValue, [CallerMemberName] string propertyName = "")
+        {
+            T? oldValue;
+            if (_properties.TryGetValue(propertyName, out object? _prop))
+            {
+                oldValue = (T?)_prop;
+            }
+            else
+            {
+                oldValue = default;
+            }
+            _properties[propertyName] = newValue;
+            Set(ref oldValue, newValue, propertyName);
+        }
+    }
+}
